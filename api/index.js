fa1854730964f9ba0e8bff9df6ddd5b1c260cc9a
skizzle010 <raiutkarsh3219@gmail.com>
2023-01-13 11:42:23 +0530

    ayiiytiuyti

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("api is running on port 5000");
 });
 
 app.get("/", (req, res) => {
