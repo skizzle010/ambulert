1782a0f516b9789f4227a9a747a01f7dc7bd8a21
Hemanth <hemanthsingh11011@gmail.com>
2023-01-13 12:43:03 +0530

    add auth-api

---
+++ auth-api/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.listen(5000, () => {
+  console.log("api is running on port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
